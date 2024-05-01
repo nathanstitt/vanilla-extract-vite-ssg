6784af516d7d851fa56772f8969db3eaf8458f07
Nathan Stitt <nathan@stitt.org>
2024-05-01 14:28:37 -0500

    demonstrate bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react'
 import vike from 'vike/plugin'
 import { UserConfig } from 'vite'
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 const config: UserConfig = {
-  plugins: [react(), vike()]
+  plugins: [vanillaExtractPlugin(), react(), vike({ prerender: true })]
 }
 
 export default config
